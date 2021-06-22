d1bd60b77d63ddd9860e91cdad87400bc83fb284
wonjin <wonjin@DESKTOP-MD6I2UJ>
2021-06-23 08:42:07 +0900

    프로젝트 시작

---
+++ wonjinStart/src/main/java/com/example/wonjinStart/WonjinStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.wonjinStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonjinStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonjinStartApplication.class, args);
+	}
+
+}
---
+++ wonjinStart/src/test/java/com/example/wonjinStart/WonjinStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wonjinStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonjinStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
